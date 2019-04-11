fdaaa00e1f575fa3fdc547ce2a2684051245b8af
Domedriver <mlaman@gmail.com>
2019-04-11 07:11:53 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended: false}))
+//app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(process.cwd() + '/public'))
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404)
+    res.sendFile(process.cwd() + '/views/404.html')
+})
+
+app.listen(3000)
+
