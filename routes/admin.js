bcbc7a1cba85d42ab6a56ed67d553c373107a6b4
Domedriver <mlaman@gmail.com>
2019-04-15 15:13:27 -0400

    added pug templates

--- routes/admin.js
+++ routes/admin.js
@@ -6,15 +6,21 @@ const rootDir = require('../util/path')
 
 const router = express.Router()
 
-// /admin/add-product
-router.get('/add-product', (req, res, next) => {    
-    //res.sendFile(process.cwd() + '/views/add-product.html') 
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+const products = [];
+
+router.get('/add-product', (req, res, next) => {        
+    res.render('add-product', {docTitle: 'The Pug Add Product Page'})
 })
 
 router.post('/add-product', (req, res) => {
-    console.log(req.body)
+    products.push({title: req.body.title})
+    console.log('admin.js ', req.body)
     res.redirect('/')
 })
 
-module.exports = router
+
+exports.routes = router;
+exports.products = products;
+
+// Used below export when just exporting router
+// module.exports = router
